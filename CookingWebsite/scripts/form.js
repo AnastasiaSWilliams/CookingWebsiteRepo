78af737afcaecf7d4762b3c6219e83a542a112a4
AnastasiaSWilliams <awilliams009@student.franciscan.edu>
2025-04-10 11:07:24 -0400

    recipe form update

---
+++ CookingWebsite/scripts/form.js
@@ -0,0 +1,7 @@
+function addIngre(){
+    document.getElementById("ingreBox").insertAdjacentHTML("beforeend", "<input type='text' id='ingredients' name='ingredients'>");
+}
+
+function addDirec(){
+    document.getElementById("direcBox").insertAdjacentHTML("beforeend", "<input type='text' id='ingredients' name='ingredients'>");
+}
